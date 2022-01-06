a4d35d27da6b71e2c1c2523f32def888ba9cffea
Anthony Wynne <12174019+ant358@users.noreply.github.com>
2022-01-06 08:31:22 +0000

    sunspots analysis started

--- time_series/tf_cert_notes/time_series_generator.py
+++ time_series/tf_cert_notes/time_series_generator.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -11,7 +12,7 @@ def plot_series(time, series, format="-", start=0, end=None, label=None):
     if label:
         plt.legend(fontsize=14)
     plt.grid(True)
-    
+
 
 def trend(time, slope=0):
     return slope * time
@@ -50,7 +51,39 @@ def impulses(time, num_impulses, amplitude=1, seed=None):
     series = np.zeros(len(time))
     for index in impulse_indices:
         series[index] += rnd.rand() * amplitude
-    return series   
+    return series
 
 
+def generate_series(time=np.arange(1460), 
+                    baseline=10, 
+                    amplitude=40, 
+                    slope=0.05, 
+                    noise_level=5):
+    """Generates a time series with a trend, seasonality, noise, and impulses
+                    
+    Arguments: 
+        time {np.array} -- time array
+        baseline {int} -- baseline value
+        amplitude {int} -- amplitude of the seasonal pattern
+        slope {float} -- slope of the trend
+        noise_level {int} -- noise level
 
+    Returns:
+        np.array -- time series
+    
+    """
+    return baseline + trend(time, slope) +\
+                      seasonality(time, period=365, amplitude=amplitude) +\
+                      white_noise(time, noise_level, seed=42)
+
+
+if __name__ == '__main__':
+    time = np.arange(4 * 365 + 1)
+    series = generate_series(time=time)
+
+    plt.figure(figsize=(10, 6))
+    plot_series(time, series)
+    plt.show()
+
+
+# %%
